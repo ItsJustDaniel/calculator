26151abd8fb7ab20939ade1645fa2a000bbf33b0
Daniel Li <danielli12750@gmail.com>
2020-05-04 16:49:53 +1000

    Updates

---
+++ precache-manifest.8f0a36078474dfda8e1b8fa96d7755bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e804543db732d3dd8d9828924eef70",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "851344dfc3c58fd3f16a",
+    "url": "/calculator/static/css/main.8f826a61.chunk.css"
+  },
+  {
+    "revision": "4e4775bada972fee7894",
+    "url": "/calculator/static/js/2.3bb27bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.3bb27bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851344dfc3c58fd3f16a",
+    "url": "/calculator/static/js/main.6b2cd07e.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
